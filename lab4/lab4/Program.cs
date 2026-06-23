4fb27168e7c8c1611ff1dddecc7cc142ff41f106
NakulKachhava <24010101116@darshan.ac.in>
2026-06-23 11:25:16 +0530

    Update Program.cs

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -16,19 +16,60 @@
             //ipb.CalculateBill();
             //opb.CalculateBill();
 
-            Console.WriteLine("Enter Name: ");
+
+            
+            // Console.WriteLine("Enter Name: ");
+            // string name = Console.ReadLine();
+
+            // EmailNotification en = new EmailNotification(name);
+            // SMSNotification sn = new SMSNotification(name);
+
+            // string em = en.GetNotificationMessage();
+            // string sm = sn.GetNotificationMessage();
+
+            // Console.WriteLine(em);
+            // Console.WriteLine(sm);
+            
+
+            
+            Console.WriteLine("Enter Type of the Product: ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name of the Product: ");
             string name = Console.ReadLine();
-
-            EmailNotification en = new EmailNotification(name);
-            SMSNotification sn = new SMSNotification(name);
-
-            string em = en.GetNotificationMessage();
-            string sm = sn.GetNotificationMessage();
-
-            Console.WriteLine(em);
-            Console.WriteLine(sm);
-
-
+            Console.WriteLine("Enter Stock of the Product: ");
+            int s = Convert.ToInt32(Console.ReadLine()); ;
+            if(t == 1)
+            {
+                GroceryStock gs = new GroceryStock(name,s);
+                Console.WriteLine("Enter Action(1 for add, 2 for sell): ");
+                int ac = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Quantity of the Product: ");
+                int q = Convert.ToInt32(Console.ReadLine());
+                if(ac == 1) 
+                {
+                    gs.AddProduct(name,q);
+                }
+                else
+                {
+                    gs.SellProduct(name,q);
+                }
+            }
+            else
+            {
+                ElectronicStock es = new ElectronicStock(name, s);
+                Console.WriteLine("Enter Action(1 for add, 2 for sell): ");
+                int ac = Convert.ToInt32(Console.ReadLine()); ;
+                Console.WriteLine("Enter Quantity of the Product: ");
+                int q = Convert.ToInt32(Console.ReadLine()); ;
+                if (ac == 1)
+                {
+                    es.AddProduct(name, q);
+                }
+                else
+                {
+                    es.SellProduct(name, q);
+                }
+            }
         }
     }
 }
